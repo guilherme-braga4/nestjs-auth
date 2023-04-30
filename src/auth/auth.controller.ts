ed47db1b46a0dfc1aefd7922744f80f750422b51
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-04-30 19:42:54 -0300

    feat: create signIn endpoint and the method to find user by email (unique)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { User, Prisma } from '@prisma/client';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dtos/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: AuthDto) {
+    return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
+
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+
+    //Implementar JWT
+    const { password, ...result } = user;
+
+    return result;
+  }
+}
