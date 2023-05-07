e2a3093d580e9dee97b932a1f4e66985fe6f8b42
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2023-05-07 15:11:22 -0300

    feat: create and set jwt guard to protect private routes and add a custom decorator to skip auth verification on public routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,20 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, SignUpDto } from './dtos/auth.dto';
+import { Public } from './decorators/isPublic';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: LoginDto) {
     return this.authService.signIn(signInDto);
   }
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('signUp')
   signUp(@Body() signUpDto: SignUpDto) {
